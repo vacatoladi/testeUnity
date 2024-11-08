fde892c08afeb36f2e6278f72f5a51c3c530a094
vacatoladi <keltondbv0@gmail.com>
2024-11-08 17:12:23 -0300

    Exercicio 2 de repetição PRONTO

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 02.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 02.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class RepeticaoEx02 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int golpe = 5;
+    int Dano = 20;
     void Start()
     {
-        
+        while (golpe >= 0)
+        {
+            if (golpe == 0)
+            {
+                Dano = 20;
+            } else
+            {
+                print("Você deu uma porrada de "+Dano+" de dano");
+                Dano += 2;
+            }
+            golpe--;
+        }
     }
 
     // Update is called once per frame
