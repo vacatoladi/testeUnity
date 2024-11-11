b09695da917c7a5b1baa0ec47bbb6cea7743946c
vacatoladi <keltondbv0@gmail.com>
2024-11-11 16:37:22 -0300

    Exercicio 7 de repetição PRONTO!

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 07.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 07.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class RepeticaoEx07 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    int[] Lotofacil = { 7, 2, 6, 5, 10, 21, 12, 9, 60, 1, 23, 45, 98, 25 };
+    int maior;
+
     void Start()
     {
-        
+        foreach(int i in Lotofacil)
+        {
+            if(maior < i)
+            {
+                maior = i;
+            }
+        }
+        print(maior);
     }
 
     // Update is called once per frame
