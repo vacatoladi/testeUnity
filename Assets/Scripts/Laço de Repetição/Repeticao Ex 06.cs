974ae7b166eb116ab8efa84dd722010509bdb5f5
vacatoladi <keltondbv0@gmail.com>
2024-11-11 15:48:32 -0300

    Exercicio 6 de repetição PRONTO!

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 06.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 06.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class RepeticaoEx06 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int numero = 0;
     void Start()
     {
-        
+        while (numero <= 10000)
+        {
+            if (numero % 5 == 0 && numero % 3 == 0)
+            {
+                print("fizz buzz");
+            } else if (numero % 5 == 0)
+            {
+                print("buzz");
+            } else if(numero % 3 == 0)
+            {
+                print("fizz");
+            } else
+            {
+                print(numero);
+            }
+            numero++;
+        }
     }
 
     // Update is called once per frame
