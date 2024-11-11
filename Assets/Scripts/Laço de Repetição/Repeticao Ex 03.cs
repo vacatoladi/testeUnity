4ce2ab72534654e6a5e968e2b82e052cd419f351
vacatoladi <keltondbv0@gmail.com>
2024-11-11 15:16:00 -0300

    Exercicio 3 de repeticao PRONTO!

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 03.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 03.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class RepeticaoEx03 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    int fases = 10;
+    int moedas = 0;
+
     void Start()
     {
-        
+        while (fases > 0)
+        {
+            moedas += 3;
+            if (fases == 1)
+            {
+                print("Você coletou no total de "+moedas+" moedas");
+            }
+            fases--;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Laço de Repetição/Repeticao Ex 04.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 04.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class RepeticaoEx04 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int combos = 7;
+    int pontuacao;
     void Start()
     {
-        
+        while (combos >= 0)
+        {
+            pontuacao += 10;
+            if (combos == 0)
+            {
+                print("pontuação total");
+            }
+            combos --;
+        }
     }
 
     // Update is called once per frame
