4a0fa340b4eae824313bb81137c7eeba7999fee0
vacatoladi <keltondbv0@gmail.com>
2024-11-11 15:38:13 -0300

    Exercicio 5 de repetição PRONTO!

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 05.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 05.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class RepeticaoEx05 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    int dificuldade;
+    int fases = 1;
+
+
     void Start()
     {
-        
+        while (fases <= 6)
+        {
+            dificuldade += 5;
+            
+            print("Fase "+ fases +" finalizada, dificuldade agora em "+dificuldade);
+
+            fases++;
+        }
     }
 
     // Update is called once per frame
