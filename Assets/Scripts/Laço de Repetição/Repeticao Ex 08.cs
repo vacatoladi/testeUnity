5911f9be70e1a5700fe8089915e28273c077918e
vacatoladi <keltondbv0@gmail.com>
2024-11-11 16:47:00 -0300

    Exercicio 8 de repetição PRONTO!

--- Assets/Scripts/Laço de Repetição/Repeticao Ex 08.cs
+++ Assets/Scripts/Laço de Repetição/Repeticao Ex 08.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class RepeticaoEx08 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] string frase = "";
+    string novaFrase;
+
     void Start()
     {
         
+        for (int i = frase.Length;i > 0;i--)
+        {
+            novaFrase += frase[i - 1];
+        }
+        print(novaFrase);
     }
 
     // Update is called once per frame
